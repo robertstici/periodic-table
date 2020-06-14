d065e8904acb2b69e13f78ef8b4872f334dd7473
Robert Stici <robert.stici@gmail.com>
2020-06-14 14:45:08 +0300

    Updates

---
+++ precache-manifest.a1b012e21a6b0491018b0a17fb3fdd82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f29105640e4fc6b56a100c4e817e8f22",
+    "url": "/periodic-table/index.html"
+  },
+  {
+    "revision": "8859b9d5ee93dc6bd6bb",
+    "url": "/periodic-table/static/css/main.54d2ecc4.chunk.css"
+  },
+  {
+    "revision": "68168c59d5e0b000a1f0",
+    "url": "/periodic-table/static/js/2.dbc25955.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/periodic-table/static/js/2.dbc25955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8859b9d5ee93dc6bd6bb",
+    "url": "/periodic-table/static/js/main.7f0070a9.chunk.js"
+  },
+  {
+    "revision": "87bfa1ff8fc41121541b",
+    "url": "/periodic-table/static/js/runtime-main.fb13ef13.js"
+  },
+  {
+    "revision": "c7d65a4e0fb77f8e2e60fc87d0beaa06",
+    "url": "/periodic-table/static/media/information-info-svgrepo-com.c7d65a4e.svg"
+  }
+]);
